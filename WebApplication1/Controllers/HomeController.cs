8f167c49fcba3d8ad151a99856d882899bd2124b
student <student@kr.step>
2019-12-10 20:35:14 +0200

    1st task

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private List<Book> books = new List<Book>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Book book)
+        {
+            books.Add(book);
+            ViewBag.QuantityBooks = books.Count;
+            return View("Books", ViewBag.QuantityBooks);
+        }
+    }
+}
---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public double Price { get; set; }
+    }
+}
